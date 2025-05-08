7b8f8d71f27e886ca9aa3a15e45d3220c91012c5
youshengyouse <19722008@163.com>
2025-05-08 17:23:08 +0800

    修改next.config.js,增加distdir

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ const config = {
   // basePath: "/src/app",   // 我需要部署在子路径下
   // basePath: process.env.PAGES_BASE_PATH,
   output: "export",       // Next.js 启用静态导出
+  distDir: 'docs',
   // trailingSlash: true,    // 针对 Nginx，静态文件使用 xxx/index.html 格式
   // images: {
   //   unoptimized: true,    // 静态导出必须关闭图片优化
